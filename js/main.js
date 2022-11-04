88fa96b98d778e0fcff2e01453e3f41f64e5d135
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-11-04 16:10:55 +0100

    Aggiunto ciclo per generare 5 numeri randomici

---
+++ js/main.js
@@ -0,0 +1,26 @@
+`use strict`
+
+// Visualizzare in pagina 5 numeri casuali da 1 a 100 senza duplicati.
+// Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+//FUNZIONI
+
+//creo una funzione che va a generare numeri casuali
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+//MAIN
+
+const numberArray = [];
+
+while (numberArray.length < 5) {
+
+    let numberRandom = getRndInteger(1, 100);
+    if (numberArray.includes(numberRandom) === false) {
+        numberArray.push(numberRandom);
+    }
+}
+console.log(numberArray);
